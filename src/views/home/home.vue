87a780a51bf158e429d0781d0b7a5a7211415ab4
liuwk <1834183435@qq.com>
2019-07-30 18:21:14 +0800

    修改首页

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,41 +1,45 @@
 <template>
   <div class="home">
-    <div class="carousel">
-      <el-carousel :interval="5000" arrow="always">
-        <el-carousel-item v-for="item in items" :key="item">
-          <img :src="item.src" alt="">
-        </el-carousel-item>
-      </el-carousel>
-    </div>
-    <div class="control">
-      <h3>个人资料简介</h3>
-      <p class="info">刘伟坤，90后前端开发人员，广东五华人，出厂26年有余，此博客站长，地地道道农村出生，已有小姐姐青睐。</p>
-      <div class="contact">
-        <div class="weixin">
-          <i class="iconfont icon-weixin"></i>
-        </div>
-        <div class="qq">
-          <i class="iconfont icon-icon"></i>
-        </div>
-        <div class="github">
-          <i class="iconfont icon-github"></i>
-        </div>
-        <div class="weibo">
-          <i class="iconfont icon-weibo"></i>
-        </div>
+    <div class="left">
+      <div class="carousel">
+        <el-carousel :interval="5000" arrow="always">
+          <el-carousel-item v-for="item in items" :key="item">
+            <img :src="item.src" alt="">
+          </el-carousel-item>
+        </el-carousel>
       </div>
-      <div class="statistics">
-        <div>
-          <p>文章</p>
-          <p>10</p>
+    </div>
+    <div class="right">
+      <div class="control">
+        <h3>个人资料简介</h3>
+        <p class="info">刘伟坤，90后前端开发人员，广东五华人，出厂26年有余，此博客站长，地地道道农村出生，已有小姐姐青睐。</p>
+        <div class="contact">
+          <div class="weixin">
+            <i class="iconfont icon-weixin"></i>
+          </div>
+          <div class="qq">
+            <i class="iconfont icon-icon"></i>
+          </div>
+          <div class="github">
+            <i class="iconfont icon-github"></i>
+          </div>
+          <div class="weibo">
+            <i class="iconfont icon-weibo"></i>
+          </div>
         </div>
-        <div>
-          <p>留言</p>
-          <p>34</p>
-        </div>
-        <div>
-          <p>标签</p>
-          <p>5</p>
+        <div class="statistics">
+          <div>
+            <p>文章</p>
+            <p>10</p>
+          </div>
+          <div>
+            <p>留言</p>
+            <p>34</p>
+          </div>
+          <div>
+            <p>标签</p>
+            <p>5</p>
+          </div>
         </div>
       </div>
     </div>
@@ -72,9 +76,8 @@
   .home {
     display: flex;
     margin-top: 20px;
-    height: 300px;
 
-    .carousel {
+    .left {
       flex: 3;
       margin-right: 20px;
 
@@ -92,93 +95,94 @@
       }
     }
 
-    .control {
-      position: relative;
+    .right {
       flex: 1;
-      height: 100%;
-      border-radius: 4px;
-      box-shadow: 0 0 10px rgba(37, 45, 51, 0.5);
 
-      h3 {
-        padding: 20px 0 10px 20px;
-        font-size: 16px;
-        color: #188ae2;
-      }
+      .control {
+        position: relative;
+        height: 300px;
+        border-radius: 4px;
+        box-shadow: 0 0 10px rgba(37, 45, 51, 0.5);
 
-      .info {
-        text-align: center;
-        text-indent: 2em;
-        font-size: 15px;
-        padding: 10px;
-        color: #999;
-      }
+        h3 {
+          padding: 20px 0 10px 20px;
+          font-size: 16px;
+          color: #188ae2;
+        }
 
-      .contact {
-        padding: 20px;
-        text-align: center;
-
-        div {
-          display: inline-block;
-          width: 35px;
-          height: 35px;
-          line-height: 35px;
-          margin: 0 10px;
-          border: 1px solid #1f2d3d;
-          border-radius: 50%;
+        .info {
           text-align: center;
-          cursor: pointer;
+          text-indent: 2em;
+          font-size: 15px;
+          padding: 10px;
+          color: #999;
+        }
 
-          i {
-            font-size: 20px;
+        .contact {
+          padding: 20px;
+          text-align: center;
+
+          div {
+            display: inline-block;
+            width: 35px;
+            height: 35px;
+            line-height: 35px;
+            margin: 0 10px;
+            border: 1px solid #1f2d3d;
+            border-radius: 50%;
+            text-align: center;
+            cursor: pointer;
+
+            i {
+              font-size: 20px;
+            }
+          }
+
+          .weixin {
+            color: #248a34;
+            border: 1px solid #248a34;
+          }
+
+          .qq {
+            color: #4e91d1;
+            border: 1px solid #4e91d1;
+          }
+
+
+          .weibo {
+            color: #d28300;
+            border: 1px solid #d28300;
           }
         }
 
-        .weixin {
-          color: #248a34;
-          border: 1px solid #248a34;
-        }
+        .statistics {
+          border-top: 1px solid #eee;
+          width: 100%;
+          display: flex;
+          position: absolute;
+          height: 60px;
+          line-height: 25px;
+          bottom: 0;
+          left: 0;
 
-        .qq {
-          color: #4e91d1;
-          border: 1px solid #4e91d1;
-        }
+          div {
+            padding: 5px;
+            flex: 1;
+            border-left: 1px solid #eee;
+            text-align: center;
+            font-size: 14px;
 
-
-        .weibo {
-          color: #d28300;
-          border: 1px solid #d28300;
-        }
-      }
-
-      .statistics {
-        border-top: 1px solid #eee;
-        width: 100%;
-        display: flex;
-        position: absolute;
-        height: 60px;
-        line-height: 25px;
-        bottom: 0;
-        left: 0;
-
-        div {
-          padding: 5px;
-          flex: 1;
-          border-left: 1px solid #eee;
-          text-align: center;
-          font-size: 14px;
-
-          p:nth-child(2) {
-            color: #999;
+            p:nth-child(2) {
+              color: #999;
+            }
           }
-        }
 
-        div:nth-child(1) {
-          border: none;
+          div:nth-child(1) {
+            border: none;
+          }
         }
       }
     }
-
-
   }
 
 </style>
