868978e0070ae8b70d7fb71687fc6e3474bfca03
liuwk <1834183435@qq.com>
2019-09-02 19:22:41 +0800

    博客第一版上线  之增加 修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,26 @@
 <template>
   <div id="app">
     <home :isTop="isTop"></home>
-
     <div class="bg" :style="{backgroundImage: 'url('+bgUrl+')'}">
       <transition name="fade-transform" mode="out-in">
         <router-view class="animated"></router-view>
       </transition>
-
     </div>
-    <div class="copyright">
-      <div>
-        <span>Copyright © 刘伟坤 - 博客 版权所有 Powered By liuwk Design.   </span>
-        <a target="_blank" href="http://www.beian.miit.gov.cn"><img src="./assets/img/icp1.png"> 粤ICP备19106134号</a>
-        <a target="_blank" style="color: #188ae2" href="https://github.com/itliuwk"><i class="iconfont icon-github"></i></a>
-      </div>
-      <div style="padding-top: 20px;">
-        <a href="https://www.yunaq.com/new_analytics/report/login/?site=www.sxitw.cn" title="创宇云安全" target="_blank"><img
-          src="./assets/img/jsl.png" width="127px" height="47px" style="margin:0px 5px;"></a>
-        <a href="http://www.qcloud.com/redirect.php?redirect=1001&amp;cps_key=8c96bc9f7c143bf40b60cfd558885a8d"
-           title="腾讯云安全认证" target="_blank"><img src="./assets/img/qcloud.png" width="127px" height="47px"
-                                                style="margin:0px 5px;"></a>
-      </div>
-    </div>
+    <copyright></copyright>
     <div id="top-img"></div>
   </div>
 </template>
 
 <script>
     import home from '@/components/index'
+    import copyright from '@/components/copyright'
     import {random_photo} from '@/utils/index'
 
     export default {
         name: 'App',
         components: {
-            home
+            home,
+            copyright
         },
         data() {
             return {
@@ -137,10 +124,5 @@
     Z-index: 9999;
   }
 
-  .copyright {
-    text-align: center;
-    border-top: 1px solid #eaeaea;
-    /*box-shadow: 0 0 2px rgba(37, 45, 51, 0.5);*/
-    padding: 20px 0;
-  }
+
 </style>
---
+++ src/components/copyright.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="copyright">
+    <div>
+      <span>Copyright © 刘伟坤 - 博客 版权所有 Powered By liuwk Design.   </span>
+      <a target="_blank" href="http://www.beian.miit.gov.cn"><img src="../assets/img/icp1.png"> 粤ICP备19106134号</a>
+      <a target="_blank" style="color: #188ae2" href="https://github.com/itliuwk"><i
+        class="iconfont icon-github"></i></a>
+    </div>
+    <div style="padding-top: 20px;">
+      <a href="https://www.yunaq.com/new_analytics/report/login/?site=www.sxitw.cn" title="创宇云安全" target="_blank"><img
+        src="../assets/img/jsl.png" width="127px" height="47px" style="margin:0px 5px;"></a>
+      <a href="http://www.qcloud.com/redirect.php?redirect=1001&amp;cps_key=8c96bc9f7c143bf40b60cfd558885a8d"
+         title="腾讯云安全认证" target="_blank"><img src="../assets/img/qcloud.png" width="127px" height="47px"
+                                              style="margin:0px 5px;"></a>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "copyright",
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .copyright {
+    text-align: center;
+    border-top: 1px solid #eaeaea;
+    /*box-shadow: 0 0 2px rgba(37, 45, 51, 0.5);*/
+    padding: 20px 0;
+  }
+
+</style>
