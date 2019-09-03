50929c8334626a2f1f264ffcb551b2d2783ad64b
liuwk <1834183435@qq.com>
2019-09-03 15:00:15 +0800

    博客第一版上线  之增加 关于我的 页面 --未完成

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -3,15 +3,15 @@
 
     <div class="layui-tab">
       <ul class="layui-tab-title">
-        <li lay-id="1" @click="layClick(1)" :class="active==1?'layui-this':''">关于博客</li>
-        <li lay-id="2" @click="layClick(2)" :class="active==2?'layui-this':''">关于作者</li>
+        <li lay-id="1" @click="layClick(1)" :class="active==1?'layui-this':''">关于本站</li>
+        <li lay-id="2" @click="layClick(2)" :class="active==2?'layui-this':''">关于本人</li>
         <li lay-id="3" @click="layClick(3)" :class="active==3?'layui-this':''">友情链接</li>
         <li lay-id="4" @click="layClick(4)" :class="active==4?'layui-this':''">留言墙</li>
       </ul>
     </div>
 
     <div v-show="active==1" class="layui-content" style="text-align: center">
-      <img src="../../assets/img/author.png" alt="">
+      <img class="favicon" src="../../assets/img/favicon.png" alt="" style="border: 1px solid #ccc">
       <h3>随心博客</h3>
       <p>前端开发人员的个人博客，记录工作上，生活上的知识，并分享前端方面技术和一些趣事。</p>
       <p>www.sxitw.cn</p>
@@ -26,10 +26,30 @@
         <a target="_blank" href="https://koajs.com/">koa2</a>
         +
         <a target="_blank" href="https://www.mysql.com/">mysql</a>，
-        <a target="_blank" href="https://buy.cloud.tencent.com/cvm?tab=lite&loginSet=SET_PASSWORD">linux CentOS</a> 部署， 基本功能已经完成，后续将会继续完善功能和页面的优化！
+        <a target="_blank" href="https://buy.cloud.tencent.com/cvm?tab=lite&loginSet=SET_PASSWORD">linux CentOS</a> 部署，
+        基本功能已经完成，后续将会继续完善功能和页面的优化！
       </p>
-      <p>本博客所有前后端代码已放在 <a style="color: #188ae2" href="https://github.com/itliuwk">Github</a></p>
+      <p>本博客所有前后端代码已放在 <a target="_blank" style="color: #188ae2" href="https://github.com/itliuwk">Github</a></p>
       <h3 style="text-align:center;margin-top: 20px;">end</h3>
+    </div>
+
+
+    <div v-show="active==2" class="layui-content" style="text-align: center">
+      <img class="author" src="../../assets/img/author.png" alt="">
+      <h3>刘伟坤</h3>
+      <p class="info">
+        90后 <strong>前端开发</strong> 大军中的一员，懂点Node知识，本博客后台基于
+        <a target="_blank" href="https://koajs.com/">koa2</a>
+        +
+        <a target="_blank" href="https://www.mysql.com/">mysql</a>搭建
+      </p>
+      <p class="synopsis">
+        <span>简介</span>
+      </p>
+      <p class="info">
+        刘伟坤，90后前端开发人员，广东五华人，出厂26年有余，此博客站长，地地道道农村出生，已有小姐姐青睐和一个可爱的儿子。
+      </p>
+      <img class="son" src="../../assets/img/son.png" alt="">
     </div>
 
 
@@ -47,7 +67,7 @@
         name: "about",
         data() {
             return {
-                active: 1
+                active: 2
             }
         },
         components: {
@@ -88,13 +108,13 @@
     height: 100%;
     min-height: 900px;
     box-shadow: 0 0 10px 2px #666;
-    padding-top: 10px;
 
     .layui-tab {
       background: #fff;
       width: 100%;
       margin-left: auto;
       margin-right: auto;
+      padding-top: 20px;
 
       .layui-tab-title {
         text-align: center;
@@ -120,10 +140,16 @@
       background: #fff;
       padding-top: 20px;
 
-      img {
+      .favicon,.author {
         width: 120px;
         height: 120px;
         border-radius: 50%;
+      }
+      .son{
+        width: 40%;
+        height: 500px;
+        border-radius: 10px;
+        margin: 20px 0;
       }
 
       p {
